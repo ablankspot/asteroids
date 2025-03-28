e6330274fb603839031da006260376916621a12b
ablankspot <ablankspot@gmail.com>
2025-03-27 17:17:20 -0700

    Add initial logic to draw the player

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
   pygame.init()
@@ -11,6 +12,10 @@ def main():
   print(f'Screen height: { SCREEN_HEIGHT }')
 
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  clock = pygame.time.Clock()
+  dt = 0
+
+  player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
   while True:
     for event in pygame.event.get():
@@ -18,7 +23,11 @@ def main():
         return
      
     screen.fill((0, 0, 0))
+    player1.draw(screen)
+    
     pygame.display.flip()
+    dt = clock.tick(60) / 1000
+    
 
 if __name__ == "__main__":
     main()
